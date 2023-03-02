8edb7d9a60b1ebe10b721131776c2264ec6d8e05
jasmeen1234 <jasmeenbano12@gmail.com>
2023-03-03 00:02:08 +0530

    submit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+   this.state= {
+    name: "John Doe", enrollmentNo: "12345678", age: 34
+  };
+}
+changeAge=()=>{
+  this.setState({age: this.state.age+1});
+}
+render() {
+  return(
+    <div>
+<h1>Hello, my name is {this.state.name}</h1>
+  <p>I am {this.state.age} years old and my enrollment no is {this.state.enrollmentNo}</p>
+<button onClick={this.changeAge}>click</button>
+</div>
+);
+}
 }
 
-export default App;
+
+export default App;
